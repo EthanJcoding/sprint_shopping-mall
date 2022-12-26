0d21690c8feca47fe2ebc11f6a0afbfd5b501d4c
[firstname lastname] <[valid-email]>
2022-12-26 16:41:44 +0900

    feat: @commons 파일추가

---
+++ client/components/@commons/Header/Header_bar.tsx
@@ -0,0 +1,15 @@
+function Header() {
+    return (
+      <div className="bg-white shadow rounded flex justify-content: space-around">
+        <h1>
+          ABOUT
+        </h1>
+        <button>로고</button>
+        <h1>
+            장바구니
+        </h1>
+      </div>
+    );
+  }
+
+  export default Header;
