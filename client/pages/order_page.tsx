73fe738b3535ec17bd78a67fccdeee4104ccdc17
[firstname lastname] <[valid-email]>
2023-01-05 16:23:45 +0900

    pull 전 commit

--- client/pages/order_page.tsx
+++ client/pages/order_page.tsx
@@ -11,7 +11,6 @@ export default function order_page() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <Layout>
         <Order />
       </Layout>
