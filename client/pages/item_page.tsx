d660c6b276b5aec43f06437a53e38b05fdfb1a86
[firstname lastname] <[valid-email]>
2023-01-19 17:33:28 +0900

    fix: 코드 수정 2차

--- client/pages/item_page.tsx
+++ client/pages/item_page.tsx
@@ -6,7 +6,7 @@ export default function item_page() {
   return (
     <>
       <Head>
-        <title>명환님 우리 취업 화이팅..!</title>
+        <title>filll Shoping_mall</title>
         <meta name="description" content="Nextjs 뿌시기" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
